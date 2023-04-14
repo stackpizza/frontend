7a33b8026b8c98eb9c8c7f9eaec693baa17f6714
Danhalf <danhel@rocketmail.com>
2023-04-14 22:56:15 +0300

    fix:menu item alt attribute

--- src/common/modules/menu/components/menu-item/menu-item.component.tsx
+++ src/common/modules/menu/components/menu-item/menu-item.component.tsx
@@ -12,7 +12,7 @@ export const MenuItem: FC<IMenuItemProps> = ({ imagePath, weight, pizzaName, ing
   return (
     <div className="w-96 shadow-xl roundex-xl bg-white">
       <div className="relative">
-        <img className="w-full rounded-t-2xl object-cover object-center h-[247px]" src={imagePath} alt="{pizzaName} photo" />
+        <img className="w-full rounded-t-2xl object-cover object-center h-[247px]" src={imagePath} alt={pizzaName} />
         <span className="rounded-[32px] absolute bottom-1.5 right-3 bg-gray-900/50 text-white text-sm px-2 py-0.5">{weight}g</span>
       </div>
       <div className="p-8">
